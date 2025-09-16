17a47bf65cf39f8c6493479d6dfbf8ba04fce72c
TOP-FDesmet <florian.desmet@topchretien.com>
2025-09-16 09:57:13 +0200

    first step: create random string

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 import { prompt } from "./prompt.js";
 
-const name = prompt("Comment t'appeles tu ? ");
+const MAX_LENGTH = 36;
+const MIN_LENGTH = 8;
 
-console.log(`Salut ${name}`);
+const numberOfCharacters = prompt("🔢 Combien de caractères ? (8-36)\n");
+
+const createRandomString = (length) => {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  console.log(chars.length);
+  return result;
+};
+
+const password = createRandomString(numberOfCharacters);
+
+console.log(`Nouveau mot de passe: ${password}`);
